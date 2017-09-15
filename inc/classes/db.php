719ef159152251ceba46a43d52ebab0f275a932b
Donnie <dcanimation@gmail.com>
2017-09-15 16:48:40 -0500

    added functions and db file

---
+++ inc/classes/db.php
@@ -0,0 +1,30 @@
+<?php
+
+class DB {
+	
+	protected static $con;
+	
+	private function __construct() {
+		try {
+			self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=8080;dbname=login_course', 'root', 'root');
+			self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
+		} catch (PDOException $e) {
+			echo "Could not connect to database. \r\n";
+			exit;
+		}
+		
+	}
+	
+	public static function getConnection() {
+		// if this instance has not been started, start it
+		if (!self::con) {
+			new DB();
+		}
+		
+		// return the writeable db connection
+		return self::con;
+	}
+}
+
+?>
